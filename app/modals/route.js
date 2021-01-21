a1b93bf844d59c3dab62ee2c1c2fcc979f2b459d
Mohd Imran <imranrcazy200@gmail.com>
2021-01-21 15:33:01 +0530

    added more field for setting feature routes

--- app/modals/route.js
+++ app/modals/route.js
@@ -1,48 +1,62 @@
-const uuid = require('uuid');
+const uuid = require("uuid");
 
 const Route = {
-    document_id: null,
-    page_url: null,
-    page_title: null,
-    page_description: null,
-    page_keywords: null,
-    page_offer_title: null,
-    page_offer_description: null,
-    route_name: null,
-    created_at: null,
-    updated_at: null,
+  document_id: null,
+  page_url: null,
+  page_title: null,
+  page_description: null,
+  page_keywords: null,
+  page_offer_title: null,
+  page_offer_description: null,
+  route_name: null,
+  from_name: null,
+  to_name: null,
+  distance: null,
+  sedan_base_fare: null,
+  suv_base_fare: null,
+  hatchback_base_fare: null,
+  sedan_state_tax: null,
+  suv_state_tax: null,
+  hatchback_state_tax: null,
+  gst: null,
+  toll_amount: null,
+  driver_allowance: null,
+  waiting_charge: null,
+  extra_km: null,
+  created_at: null,
+  updated_at: null,
 };
 
 function returnUpdatedRouteCollectionField(receivedData) {
-    const newObj = Object.assign({}, Route);
-    Object.keys(newObj).forEach((index) => {
-        newObj[index] = receivedData[index] ? receivedData[index] : newObj[index];
-    });
-    return newObj;
+  const newObj = Object.assign({}, Route);
+  Object.keys(newObj).forEach((index) => {
+    newObj[index] = receivedData[index] ? receivedData[index] : newObj[index];
+  });
+  return newObj;
 }
 
 function returnNewRouteData(receivedData) {
-    const newObj = Object.assign({}, Route);
-    Object.keys(newObj).forEach((index) => {
-        newObj[index] = receivedData[index] ? receivedData[index] : newObj[index];
-    });
-    newObj['document_id'] = uuid();
-    newObj['created_at'] = (new Date()).getTime();
-    newObj['updated_at'] = (new Date()).getTime();
-    return newObj;
+  const newObj = Object.assign({}, Route);
+  Object.keys(newObj).forEach((index) => {
+    newObj[index] = receivedData[index] ? receivedData[index] : newObj[index];
+  });
+  newObj["document_id"] = uuid();
+  newObj["created_at"] = new Date().getTime();
+  newObj["updated_at"] = new Date().getTime();
+  return newObj;
 }
 
 function returnUpdatedRouteData(previousData, receivedData) {
-    const newObj = Object.assign({}, previousData);
-    Object.keys(Route).forEach((index) => {
-        newObj[index] = receivedData[index] ? receivedData[index] : newObj[index];
-    });
-    newObj['updated_at'] = (new Date()).getTime();
-    return newObj;
+  const newObj = Object.assign({}, previousData);
+  Object.keys(Route).forEach((index) => {
+    newObj[index] = receivedData[index] ? receivedData[index] : newObj[index];
+  });
+  newObj["updated_at"] = new Date().getTime();
+  return newObj;
 }
 
 module.exports = {
-    returnUpdatedRouteCollectionField: returnUpdatedRouteCollectionField,
-    returnNewRouteData: returnNewRouteData,
-    returnUpdatedRouteData: returnUpdatedRouteData,
+  returnUpdatedRouteCollectionField: returnUpdatedRouteCollectionField,
+  returnNewRouteData: returnNewRouteData,
+  returnUpdatedRouteData: returnUpdatedRouteData,
 };
