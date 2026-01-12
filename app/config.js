75ab769ac1cde14d9ccc374e3ea97ef033526fbe
Amit Dubey <amitdubey8888@gmail.com>
2026-01-12 20:10:15 +0530

    Security: Refactor secrets to use environment variables

--- app/config.js
+++ app/config.js
@@ -5,31 +5,21 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
+require('dotenv').config();
+
 module.exports = {
   admin: admin,
   db: db,
-  server_secret: '$2y$12$4A2phQdjCl9fFvcuTHpn3.l7mZTBNagz4HrApfnY1tJYcyHgalifK',
-  token_expire: '30d',
-  mail: 'info@nsgtaxi.com',
-  google_api_key: 'AIzaSyCJnen-AB1eM6aoGKKLSnA1cyDIQknzD5Q',
-  google_server_key: 'AIzaSyCnuv7piwyIesoSSUE7Aj3H-siKcEQUWjg',
-
-  //PayTM Testing Credentials
-  // request_type: 'Payment',
-  // merchant_id: 'nUuswO43569342193043',
-  // merchant_key: 'R86OGnmG7ippgq_m',
-  // website_name: 'WEBSTAGING',
-  // currency: 'INR',
-  // hostname: 'securegw-stage.paytm.in',
-  // transaction_url: 'https://securegw-stage.paytm.in',
-  // transaction_order_process_url: 'https://securegw-stage.paytm.in/order/process',
-  // transaction_order_status_url: 'https://securegw-stage.paytm.in/order/status',
-  // callback_url: 'http://localhost:3000/payment-status',
+  server_secret: process.env.SERVER_SECRET,
+  token_expire: process.env.TOKEN_EXPIRE,
+  mail: process.env.MAIL,
+  google_api_key: process.env.GOOGLE_API_KEY,
+  google_server_key: process.env.GOOGLE_SERVER_KEY,
 
   //PayTM Live Credentials
   request_type: 'Payment',
-  merchant_id: 'RrjpTX26296194961608',
-  merchant_key: 'Oip_i7cYPlwXFmqS',
+  merchant_id: process.env.PAYTM_MERCHANT_ID,
+  merchant_key: process.env.PAYTM_MERCHANT_KEY,
   website_name: 'DEFAULT',
   currency: 'INR',
   hostname: 'securegw.paytm.in',
@@ -38,19 +28,9 @@ module.exports = {
   transaction_order_status_url: 'https://securegw.paytm.in/order/status',
   callback_url: 'https://www.nsgtaxi.com/payment-status',
 
-  // Razorpay Testing Credentials
-  // key: 'rzp_test_bLWnHfbTJQWcfw',
-  // secret: 'JR0Uvtg45vuY00NYK5q6HcTa',
-  // name: 'NSGA TRAVELS PVT LTD',
-  // description: 'We plan for your travel!',
-  // image: 'https://www.nsgtaxi.com/assets/images/company/logo-110-40.png',
-  // url: 'http://localhost:3000/payment-status',
-  // address: `Shop No. 209, Central Arcade, DLF-2, Gurugram, Haryana 122002 - India`,
-  // color: '#F78536'
-
   // Razorpay Live Credentials
-  key: 'rzp_live_nAYX5ZDKcQwWGF',
-  secret: '13z11vEyS4lyO3jC3v1FJyes',
+  key: process.env.RAZORPAY_KEY,
+  secret: process.env.RAZORPAY_SECRET,
   name: 'NSGA TRAVELS PVT LTD',
   description: 'We plan for your travel!',
   image: 'https://www.nsgtaxi.com/assets/images/company/logo-110-40.png',
